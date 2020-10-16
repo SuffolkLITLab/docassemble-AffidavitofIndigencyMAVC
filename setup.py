de87bdbe1e984f788cf33f3c357dd2279b900074
System Administrator <qsteenhuis@gmail.com>
2020-10-16 15:03:28 -0400

    Rename attachment variable

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.AffidavitofIndigencyMAVC',
-      version='0.0.13',
+      version='0.0.14',
       description=('A docassemble extension.'),
       long_description='# docassemble.AffidavitofIndigencyMAVC\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
